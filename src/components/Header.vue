6a978723e73f2369c2d26c3e82dc5a1cb8925162
Gtp87 <fabio.giannitrapani@gmail.com>
2022-01-24 13:46:59 +0100

    fix header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -21,12 +21,20 @@
             </div>
         </div>
     </div>
-        <div class="jumbo-container">
+        <div class="jumbo-container-top">
             <div class="container-70">
-                <div class="jumbo-text">
-                    <h1 class="display-1">Tech Forum!</h1>
+                <div class="jumbo-text-top">
+                    <h1 class="display-1 fw-bold">Tech Forum!</h1>
                     <p>Discuss all the latest technology news and trends</p>
-                    <button type="button" class="btn btn-primary">GET AVADA NOW!</button>
+                    <button type="button" class="btn btn-primary">GET AVADA NOW! > </button>
+                </div>
+            </div>
+        </div>
+        <div class="jumbo-container-bottom">
+            <div class="container-50">
+                <div class="jumbo-text-bottom text-center">
+                    <h6 class="text-uppercase font-monospace">avada forum is the place to be</h6>
+                    <h5 class="fw-light fs-5 lh-base">In qui lectus sed leo elementum faucibus in dapibus dictum. <br> Nulla molestie tortor nec lectus venenatis, sed blandit dui finibus. <br> dolor at, bibendum purus. Donec vehicula nec tortor ac finibus</h5>
                 </div>
             </div>
         </div>
@@ -45,10 +53,6 @@ export default {
 header {
     color: white;
     background-color: $headerBgColor;
-    .container-70 {
-        width: 70%;
-        margin: auto;
-    }
     .logo {
         padding: 0.6em;
     }
@@ -57,17 +61,28 @@ header {
         margin: 0 1em;
         cursor: pointer;
     }
-    .jumbo-container {
+    .jumbo-container-top {
         background-image: url(../assets/img/home_slider_bg.jpg);
         background-position: center;
         background-size: cover;
         background-repeat: no-repeat;
-        .jumbo-text {
-            padding-top: 30%;
+        .jumbo-text-top {
+            padding-top: 20%;
             padding-bottom: 20%;
             .btn-primary {
             background-color: $headerBgColor;
             border-color: $headerBgColor;  
+            }
+        }
+    }
+    .jumbo-container-bottom {
+        background-color: $silverChalice;
+        color: black;
+        .jumbo-text-bottom {
+            padding: 4em 0;
+            h6 {
+                font-size: 0.8em;
+                font-weight: bold;
             }
         }
     }

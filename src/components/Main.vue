572a99c2daed0f4bc3821e6db8066592347a3840
Gtp87 <fabio.giannitrapani@gmail.com>
2022-01-24 16:17:46 +0100

    working on main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,103 @@
 <template>
-  <div></div>
+<main>
+    <div class="container-50">
+        <div class="main-title text-center pt-5">
+            <h6 class="text-uppercase font-monospace">phasellus eget metus</h6>
+            <h3 class="fw-bold">All the latest news</h3>
+        </div>
+    </div>
+    <div class="container-70">
+        <div class="row row-cols-3 mt-5">
+            <div class="col news">
+                <div class="img-container">
+                    <img src="../assets/img/post_feat_img_25-320x202.jpg" alt="">
+                </div>
+                <div class="news-text">
+                    <div class="news-title">Morbi vitae dui euismod vulputate sollicitudin</div>
+                    <div class="news-date">October 11th, 2015 | 2 comments</div>
+                    <div class="news-text">Donec finibus sit amet orci eget ulticies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl</div>
+                </div>
+            </div>
+            <div class="col news">
+                <div class="img-container">
+                    <img src="../assets/img/post_feat_img_24-320x202.jpg" alt="">
+                </div>
+                <div class="news-text">
+                    <div class="news-title">Vivamus pellenteque, felis quis varius</div>
+                    <div class="news-date">October 11th, 2015 | Comments Off</div>
+                    <div class="news-text">Donec finibus sit amet orci eget ulticies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl</div>
+                </div>
+            </div>
+            <div class="col news">
+                <div class="img-container">
+                    <img src="../assets/img/post_feat_img_23-320x202.jpg" alt="">
+                </div>
+                <div class="news-text">
+                    <div class="news-title">Donec ornare pretium eget scelisque justo</div>
+                    <div class="news-date">October 11th, 2015 | Comments Off</div>
+                    <div class="news-text">Donec finibus sit amet orci eget ulticies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl</div>
+                </div>
+            </div>
+        </div>
+        <div class="featured-article">
+            <div class="feat-article-text text-light">
+                <h6 class="text-uppercase">featured article</h6>
+                <h4>Cras malesuada et orci eget pharetra</h4>
+                <p>In quis lectus sed leo elementum faucibus in dapibus dictum. Nullamolestie tortor nec lectus veneratis, sed blandit dui, dolor at bibendum sadips ets ipsums dolore ficilis uns leo lectus.</p>
+                <button type="button" class="btn btn-dark">READ MORE ></button>
+            </div>
+        </div>
+        <div class="row row-cols-3 mt-5">
+            <div class="col news">
+                <div class="img-container">
+                    <img src="../assets/img/post_feat_img_22-320x202.jpg" alt="">
+                </div>
+                <div class="news-text">
+                    <div class="news-title">Fusce sollicitudibne nunc sed placerat varius</div>
+                    <div class="news-date">October 11th, 2015 | Comments Off</div>
+                    <div class="news-text">Donec finibus sit amet orci eget ulticies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl</div>
+                </div>
+            </div>
+            <div class="col news">
+                <div class="img-container">
+                    <img src="../assets/img/post_feat_img_21-320x202.jpg" alt="">
+                </div>
+                <div class="news-text">
+                    <div class="news-title">Donec facilis sodales leo sit amet laoreet</div>
+                    <div class="news-date">October 11th, 2015 | Comments Off</div>
+                    <div class="news-text">Donec finibus sit amet orci eget ulticies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl</div>
+                </div>
+            </div>
+            <div class="col news">
+                <div class="img-container">
+                    <img src="../assets/img/post_feat_img_20-320x202.jpg" alt="">
+                </div>
+                <div class="news-text">
+                    <div class="news-title">Proin eu purus sed aru aliquet curabir vens</div>
+                    <div class="news-date">October 11th, 2015 | Comments Off</div>
+                    <div class="news-text">Donec finibus sit amet orci eget ulticies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl</div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-8">
+                <div class="reviewed-product">
+                    <div class="reviewed-product-text text-light">
+                        <h6 class="text-uppercase">reviewed product</h6>
+                        <h4>Mauris viverra atisan ipsus eget felis prims efficitur varius</h4>
+                        <p>In quis lectus sed leo elementum faucibus in dapibus dictum. Nullamolestie tortor nec lectus veneratis, sed blandit dui, dolor at bibendum sadips ets ipsums dolore ficilis uns leo lectus.</p>
+                        <button type="button" class="btn btn-dark">READ MORE ></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="tutorials mb-4">
+                    <h5>Tutorials &amp; Guides</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
 </template>
 
 <script>
@@ -10,5 +108,42 @@ export default {
 </script>
 
 <style lang="scss">
+@import "../assets/scss/style.scss";
 
+.main-title::after {
+    height: 3px;
+    width: 10%;
+    margin: auto;
+    display: block;
+    background-color: $colorSalmon;
+    content:"";
+}
+
+.featured-article {
+    background-image: url(../assets/img/featured_article_1_bg.jpg);
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    .feat-article-text {
+        padding: 20% 10% 10% 5%;
+    }
+}
+
+.reviewed-product {
+    background-image: url(../assets/img/featured_article_2_bg.jpg);
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    .reviewed-product-text {
+        padding: 20% 10% 10% 5%;
+    }
+}
+
+.tutorials::after {
+    height: 3px;
+    width: 10%;
+    display: block;
+    background-color: $colorSalmon;
+    content:"";
+}
 </style>
